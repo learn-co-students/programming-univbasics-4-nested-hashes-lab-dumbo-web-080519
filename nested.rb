2e0925a8361b4ae2b56231b28e9ff963ecf11ddf
Kimberly Bone <k_bone@uncg.edu>
2019-06-22 15:35:02 +0000

    Done.

--- nested.rb
+++ nested.rb
@@ -15,9 +15,9 @@ def hopper
           :languages => ["C"]
         }
      }
-
-
+     programmer_hash[:grace_hopper]
 end
+
 
 def alan_kay_is_known_for
 	# What combination of keys would you use to return the value of the :known_for key of :alan_kay?
@@ -37,6 +37,7 @@ def alan_kay_is_known_for
           :languages => ["C"]
         }
      }
+     programmer_hash[:alan_kay][:known_for]
 end
 
 def dennis_ritchies_language
@@ -55,7 +56,9 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
+     programmer_hash[:dennis_ritchie][:languages] = "C"
 end
+
 
 def adding_matz
 # add the following information to the top level of programmer_hash
@@ -79,8 +82,10 @@ def adding_matz
           :languages => ["C"]
         }
      }
-
-    
+     programmer_hash[:yukihiro_matsumoto] = {
+       :know_for => "Ruby", :languages => ["LISP", "C"] 
+     }
+     programmer_hash
 end
 
 def changing_alan
@@ -101,9 +106,10 @@ def changing_alan
      }
      #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
      alans_new_info = "GUI"
-     
-     
+  programmer_hash[:alan_kay][:known_for] = alans_new_info  
+  programmer_hash
 end
+
 
 def adding_to_dennis
 	programmer_hash = 
@@ -120,6 +126,7 @@ def adding_to_dennis
           :known_for => "Unix",
           :languages => ["C"]
         }
-     }
-
+ 		}
+ 		programmer_hash[:dennis_ritchie][:languages] << "Assembly"
+ 		programmer_hash
 end
