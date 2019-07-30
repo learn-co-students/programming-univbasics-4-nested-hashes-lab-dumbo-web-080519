7d56d150a5debe07058e5747a66262096db50bb1
Kenny Marks <kmarks2013@gmail.com>
2019-07-30 22:46:46 +0000

    Done.

--- nested.rb
+++ nested.rb
@@ -103,9 +103,9 @@ def changing_alan
           :languages => ["C"]
         }
      }
+  return programmer_hash
    
 end
-return changing_alan[:alan_kay][:known_for]
 
 
 def adding_to_dennis
@@ -124,4 +124,6 @@ def adding_to_dennis
           :languages => ["C"]
         }
  		}
+ programmer_hash[:dennis_ritchie][:languages].push("Assembly")
+ return programmer_hash
 end
