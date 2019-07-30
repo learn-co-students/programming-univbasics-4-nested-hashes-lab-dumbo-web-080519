f79f5b261c930adb54914c23026ccf1fca35aef9
Kenny Marks <kmarks2013@gmail.com>
2019-07-30 21:52:17 +0000

    Done.

--- nested.rb
+++ nested.rb
@@ -15,6 +15,7 @@ def hopper
           :languages => ["C"]
         }
      }
+  return programmer_hash[:grace_hopper]
 
 
 end
@@ -37,6 +38,7 @@ def alan_kay_is_known_for
           :languages => ["C"]
         }
      }
+  return programmer_hash[:alan_kay][:known_for]
 end
 
 def dennis_ritchies_language
@@ -52,18 +54,14 @@ def dennis_ritchies_language
         },
         :dennis_ritchie => {
           :known_for => "Unix",
-          :languages => ["C"]
+          :languages => "C"
         }
      }
+  return programmer_hash[:dennis_ritchie][:languages]
 end
 
 def adding_matz
-# add the following information to the top level of programmer_hash
-# :yukihiro_matsumoto => {
-#   :known_for => "Ruby",
-#    :languages => ["LISP", "C"]
-# }
-
+  
 	programmer_hash = 
  		{
         :grace_hopper => {
@@ -79,11 +77,17 @@ def adding_matz
           :languages => ["C"]
         }
      }
-
-    
+  matz = {
+ 		    :yukihiro_matsumoto => {
+ 		      :known_for => "Ruby",
+ 		      :languages => ["LISP", "C"]
+ 		    }
+  }
+ programmer_hash.merge(matz)    
 end
 
 def changing_alan
+	alans_new_info = "GUI"
 	programmer_hash = 
  		{
         :grace_hopper => {
@@ -91,7 +95,7 @@ def changing_alan
           :languages => ["COBOL", "FORTRAN"]
         },
         :alan_kay => {
-          :known_for => "Object Orientation",
+          :known_for => alans_new_info,
           :languages => ["Smalltalk", "LISP"]
         },
         :dennis_ritchie => {
@@ -99,11 +103,10 @@ def changing_alan
           :languages => ["C"]
         }
      }
-     #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
-     alans_new_info = "GUI"
-     
-     
+   
 end
+return changing_alan[:alan_kay][:known_for]
+
 
 def adding_to_dennis
 	programmer_hash = 
@@ -120,6 +123,5 @@ def adding_to_dennis
           :known_for => "Unix",
           :languages => ["C"]
         }
-     }
-
+ 		}
 end
